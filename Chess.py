5d7b9cc2273f9bf36f5182b7c57ed011e2b3fc81
Erdinc Mutlu <erdinc_mutlu@yahoo.com>
2018-03-27 19:41:23 +0100

    addMove method added

--- Chess.py
+++ Chess.py
@@ -1,5 +1,7 @@
 board = [[1] * 8 for i in range(8)]
 
+possible_moves = []
+
 def knight_moves(position, board):
     """This function shows all possible knight moves - assuming nothing else
     is on the board"""
@@ -8,49 +10,22 @@ def knight_moves(position, board):
     row = int(row) - 1
     column = ord(column) - ord('a')
     i, j = row, column
-    possible_moves = []
-
-    try:
-        move = board[i + 2][j + 1]
-        possible_moves.append([i + 2, j + 1])
-    except:
-        pass
-    try:
-        move = board[i + 2][j - 1]
-        possible_moves.append([i + 2, j - 1])
-    except:
-        pass
-    try:
-        move = board[i + 1][j - 2]
-        possible_moves.append([i + 1, j - 2])
-    except:
-        pass
-    try:
-        move = board[i + 1][j + 2]
-        possible_moves.append([i + 1, j + 2])
-    except:
-        pass
-    try:
-        move = board[i - 1][j - 2]
-        possible_moves.append([i - 1, j - 2])
-    except:
-        pass
-    try:
-        move = board[i - 1][j + 2]
-        possible_moves.append([i - 1, j + 2])
-    except:
-        pass
-    try:
-        move = board[i - 2][j - 1]
-        possible_moves.append([i - 2, j - 1])
-    except:
-        pass
-    try:
-        move = board[i - 2][j + 1]
-        possible_moves.append([i - 2, j + 1])
-    except:
-        pass
+    
+    addMove(i, j, 2, 1)
+    addMove(i, j, 2, -1)
+    addMove(i, j, 1, -2)
+    addMove(i, j, 1, 2)
+    addMove(i, j, -1, -2)
+    addMove(i, j, -1, 2)
+    addMove(i, j, -2, -1)
+    addMove(i, j, -2, 1)
     return possible_moves
 
+
+def addMove(row, col, diff_row, diff_col):
+    if row+diff_row>=0 and col+diff_col>=0: 
+        possible_moves.append([row + diff_row, col + diff_col])
+    
+    
 position = 'a4'
 print(knight_moves(position, board))
